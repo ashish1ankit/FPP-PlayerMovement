824880682aef13217be4cc813a8f9c97fc539143
ashish1ankit <32021646+ashish1ankit@users.noreply.github.com>
2020-09-13 20:56:36 +0530

    Second Commit
    Upload of Unity Project

---
+++ FightMe/Assets/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float speed=5f;
+
+    private PlayerMotor motor;
+
+    void Start(){
+        motor=GetComponent<PlayerMotor>();
+    }
+
+    void update(){
+        float _xMov=Input.GetAxisRaw("Hroizontal");
+        float _zMov=Input.GetAxisRaw("Verical");
+
+        Vector3 _movHorizontal=transform.right*_xMov;
+        Vector3 _movVertical=transform.forward*_zMov;
+
+        Vector3 _velocity=(_movHorizontal+_movVertical).normalized*speed;
+
+        motor.Move(_velocity);
+    }
+}
